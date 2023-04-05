0a7b3ca0cdeda5dc6704315a0094134144690636
Alakbarbh <alakbarbh@code.edu.az>
2023-04-05 10:32:21 -0800

    Finish task

---
+++ EntityFramework-Slider/EntityFramework-Slider/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using EntityFramework_Slider.Data;
+using EntityFramework_Slider.Models;
+using EntityFramework_Slider.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace EntityFramework_Slider.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.ToListAsync();
+
+            SliderInfo sliderInfo = await _context.SliderInfos.FirstOrDefaultAsync();
+
+            IEnumerable<Blog> blogs = await _context.Blogs.Where(m=>!m.SoftDelete).ToListAsync();
+
+            IEnumerable<Category> categories = await _context.Categories.Where(m => !m.SoftDelete).ToListAsync();
+
+            IEnumerable<Product> products = await _context.Products.Include(m=>m.Images).Where(m => !m.SoftDelete).ToListAsync();
+
+            About abouts = await _context.Abouts.Include(m=>m.Advantages).FirstOrDefaultAsync();
+
+            IEnumerable<Expert> experts = await _context.Experts.Where(m => !m.SoftDelete).ToListAsync();
+
+            Subscribe subscribes = await _context.Subscribes.FirstOrDefaultAsync();
+
+            IEnumerable<Say> says = await _context.Says.Where(m => m.SoftDelete == false).ToListAsync();
+
+            IEnumerable<Instagram> instagrams = await _context.Instagrams.Where(m => m.SoftDelete == false).ToListAsync();
+
+            HomeVM model = new()
+            {
+                Sliders = sliders,
+                SliderInfo = sliderInfo,
+                Blogs = blogs,
+                Categories = categories,
+                Products = products,
+                Abouts = abouts,
+                Experts = experts,
+                Subscribes = subscribes,
+                Says = says,
+                Instagrams = instagrams
+            };
+
+            return View(model);
+        }
+    }
+}
